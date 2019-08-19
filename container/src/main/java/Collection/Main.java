cc8af1c2e8507f2677527ab1a89bc5fee6f2a463
jdzhang <673235694@qq.com>
2019-08-19 22:54:48 +0800

    initial project

---
+++ container/src/main/java/Collection/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2019 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+package Collection;
+
+import java.util.HashMap;
+
+/**
+ * @author zhangjundong
+ * @date 2019/8/1922:32
+ */
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("hello world");
+    }
+}
